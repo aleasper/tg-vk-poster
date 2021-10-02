ca6d69129aae456d6072417c806dc1e2e4b96de2
aleasper <alexidest@gmail.com>
2021-10-03 01:01:38 +0300

    Update arrows

--- run.py
+++ run.py
@@ -34,7 +34,7 @@ def create_vk_post(vk_client, post, attachments=None, text=None):
     if text is None:
         text = post['text']
     else:
-        text = post['text'] + '\n↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓\n' + text
+        text = post['text'] + '\n⬇⬇⬇    Обновляю    ⬇⬇⬇\n' + text
 
     files = os.listdir(f'./media/{post["id"]}')
     atachs_ids = []
