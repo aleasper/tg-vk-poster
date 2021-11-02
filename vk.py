b136070efba707e0218375105f2e948db2676514
aleasper <alexidest@gmail.com>
2021-11-02 10:06:03 +0300

    Update scope

--- vk.py
+++ vk.py
@@ -9,7 +9,7 @@ from ui import auth_vk_handler
 class VkPublicPoster:
 
     def __init__(self, login, password, group_id, logger):
-        self.vk_session = vk_api.VkApi(login, password, auth_handler=auth_vk_handler, app_id=app_id)
+        self.vk_session = vk_api.VkApi(login, password, auth_handler=auth_vk_handler, app_id=app_id, scope='wall, photos')
         self.vk_session.auth()
         self.vk = self.vk_session.get_api()
         self.user_id = self.vk.users.get()[0]['id']
